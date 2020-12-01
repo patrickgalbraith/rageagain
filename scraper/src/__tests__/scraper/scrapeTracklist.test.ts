cce8520c3bbf3a91d2944444f3d6c5619e28775e
Patrick Galbraith <patrick.galbraith@sageautomation.com>
2020-12-02 08:17:46 +1030

    Improve fixture naming in tests

--- scraper/src/__tests__/scraper/scrapeTracklist.test.ts
+++ scraper/src/__tests__/scraper/scrapeTracklist.test.ts
@@ -3,19 +3,19 @@ import { scrapeTracklistHtml } from "../../scraper/scrapeTracklists"
 
 describe('scraperV1', () => {
   it('correctly scrapes variation 1', () => {
-    testParser('variation-1_sunday-night-29th-january-2017-on-abc',
+    testParser('tracklist-variation-1_sunday-night-29th-january-2017-on-abc',
       html => scrapeTracklistHtml(html))
   })
 })
 
 describe('scraperV2', () => {
   it('correctly scrapes variation 2', () => {
-    testParser('variation-2_friday-night-4-may-2018-on-abc',
+    testParser('tracklist-variation-2_friday-night-4-may-2018-on-abc',
       html => scrapeTracklistHtml(html))
   })
 
   it('correctly scrapes variation 3', () => {
-    testParser('variation-3_saturday-night-31-october-2020-on-abc-1',
+    testParser('tracklist-variation-3_saturday-night-31-october-2020-on-abc-1',
       html => scrapeTracklistHtml(html))
   })
 })
