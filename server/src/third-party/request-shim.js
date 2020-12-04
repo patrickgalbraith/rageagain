6a172b0e56a7b87db0c7f2820586d3a99e7586e7
Patrick Galbraith <patrick.galbraith@sageautomation.com>
2020-12-04 20:16:38 +1030

    Remove fetch options not suppored by cloudflare

--- server/src/third-party/request-shim.js
+++ server/src/third-party/request-shim.js
@@ -1,9 +1,6 @@
 
 const defaultOpts = {
-  cache: 'no-cache',
-  credentials: 'omit',
   redirect: 'follow',
-  referrer: 'https://www.youtube.com/',
   headers: {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
   }
