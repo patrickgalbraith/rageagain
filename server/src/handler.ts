0d1b40163185cabdff8442cc1c51e9919338ddf3
Patrick Galbraith <patrick.galbraith@sageautomation.com>
2020-12-04 20:16:54 +1030

    Add CORS support

--- server/src/handler.ts
+++ server/src/handler.ts
@@ -1,16 +1,33 @@
 import getYoutubeVideos from "./lib/youtubeProvider"
 
-const allowedReferrers = [
-  /^https?:\/\/rageagain.com/,
-  /^https?:\/\/localhost/
+const allowedOrigins = [
+  /^https?:\/\/(www\.)?rageagain.com$/,
+  /^https?:\/\/localhost$/,
+  /^https?:\/\/localhost:.*$/
 ]
 
-export async function handleRequest(request: Request): Promise<Response> {
-  const referrerMatches = allowedReferrers.some(re => re.test(request.referrer))
+const corsHeaders = {
+  "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS",
+  "Access-Control-Allow-Headers": "*",
+  "Access-Control-Max-Age": "86400"
+}
 
-  if (!referrerMatches)
-    return new Response('Bad request', { status: 400 })
+function getCorsHeaders(request: Request): Headers {
+  const { origin } = new URL(request.url)
 
+  let respHeaders: Record<string, string> = {
+    ...corsHeaders,
+    Vary: "Origin"
+  }
+
+  if (allowedOrigins.some(re => re.test(origin))) {
+    respHeaders['Access-Control-Allow-Origin'] = origin
+  }
+
+  return new Headers(respHeaders)
+}
+
+async function handleMusicVideoSearchRequest(request: Request): Promise<Response> {
   const { searchParams } = new URL(request.url)
 
   const artist = searchParams.get('artist')
@@ -19,11 +36,57 @@ export async function handleRequest(request: Request): Promise<Response> {
   if (!artist || !song)
     return new Response('Missing param', { status: 400 })
 
-  try {
-    const videoInfoList = await getYoutubeVideos(artist, song)
+  const videoInfoList = await getYoutubeVideos(artist, song)
 
-    return new Response(JSON.stringify(videoInfoList))
+  return new Response(JSON.stringify(videoInfoList))
+}
+
+function handleOptionsRequest(request: Request): Response {
+  let headers = request.headers
+
+  // Handle CORS
+  if (
+    headers.get("Origin") !== null &&
+    headers.get("Access-Control-Request-Method") !== null &&
+    headers.get("Access-Control-Request-Headers") !== null
+  ){
+    return new Response(null, {
+      headers: getCorsHeaders(request)
+    })
+  }
+
+  // Handle standard OPTIONS request.
+  // If you want to allow other HTTP Methods, you can do that here.
+  return new Response(null, {
+    headers: {
+      Allow: "GET, HEAD, OPTIONS"
+    }
+  })
+}
+
+export async function handleRequest(request: Request): Promise<Response> {
+  try {
+    if (request.method === "OPTIONS") {
+      // Handle CORS preflight requests
+      return handleOptionsRequest(request)
+    } else if (request.method === "GET") {
+      const { pathname } = new URL(request.url)
+
+      if (/\/api\/musicvideosearch\/?/.test(pathname)) {
+        return await handleMusicVideoSearchRequest(request)
+      } else {
+        return new Response(null, {
+          status: 404,
+          statusText: "Not Found",
+        })
+      }
+    } else {
+      return new Response(null, {
+        status: 405,
+        statusText: "Method Not Allowed",
+      })
+    }
   } catch (e) {
-    return new Response('Something went wrong', { status: 500 })
+    return new Response(`Something went wrong: "${e.message}"`, { status: 500 })
   }
 }
