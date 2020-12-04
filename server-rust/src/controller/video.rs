ceca674efd1a6dbb3a11e28cb106d43784e448d7
Patrick Galbraith <patrick.galbraith@sageautomation.com>
2020-12-04 20:57:12 +1030

    Remove Rust server implementation :-(

--- server-rust/src/controller/video.rs
+++
@@ -1,9 +0,0 @@
-use crate::routes::ResolveVideoRequest;
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Debug)]
-struct VideoInfo {
-    video_ids: Vec<String>,
-}
-
-pub async fn get_video(req: ResolveVideoRequest) -> Result<impl warp::Reply, warp::Rejection> {}
