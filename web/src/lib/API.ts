4571f4df13e71e575575b7034211abff1d80c365
Patrick Galbraith <patrick.j.galbraith@gmail.com>
2021-01-17 17:30:51 +1030

    Update API url

--- web/src/lib/API.ts
+++ web/src/lib/API.ts
@@ -1,6 +1,7 @@
 import { SourceInfo } from "../Types"
 
-const baseUrl = 'http://localhost:8787/' // Must have trailing slash
+//const baseUrl = 'http://localhost:8787/' // Default local dev url of ../api project
+const baseUrl = 'https://rageagain.pjgalbraith.workers.dev/' // Must have trailing slash
 
 export const getTrackSources = (artist: string, song: string): Promise<SourceInfo[]> => {
   return new Promise((resolve, reject) => {
